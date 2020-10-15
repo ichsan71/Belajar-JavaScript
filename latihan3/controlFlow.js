4ba0b2b96a5731b7b2358784bb9af6f62df829dc
ichsan71 <ichsan.kamil71@gmail.com>
2020-10-15 21:00:07 +0700

    Belajar controlflow.js

---
+++ latihan3/controlFlow.js
@@ -0,0 +1,13 @@
+alert('Mulai');
+for(var x = 1; x <= 3; x++) {
+    alert('Hello World');
+}
+
+alert('selesai');
+
+var angka = prompt('Masukan Angka : ');
+if (angka % 2 === 0) {
+    alert(angka +' adalah bilangan GENAP' );
+} else {
+    alert(angka +' adalah Bilangan Ganjil')    
+}
