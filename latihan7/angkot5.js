dd43fdfa083eb2c55aabe28338cac7ef5f13449e
ichsan71 <ichsan.kamil71@gmail.com>
2020-10-17 20:54:00 +0700

    Belajar dan latihan Pengkondisian.js

---
+++ latihan7/angkot5.js
@@ -0,0 +1,16 @@
+var jmlAngkot = 10;
+var angkotBeroperasi= 6;
+
+for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+    
+    if (noAngkot <= angkotBeroperasi ) {
+        console.log('Angkot No. ' + noAngkot + ' Beroperasi dengan baik');
+    }
+    else if(noAngkot === 8 || noAngkot === 10) {
+        console.log('Angkot No. ' + noAngkot + ' Lembur');
+    }
+    else{
+        console.log('Angkot No. ' + noAngkot + ' Tidak beroperasi');
+    }
+}
+
